8425e0ff6faf57ac83a827ddb6098e8cb9f84837
Lucifer <luciferxhnh@gmail.com>
2025-07-19 22:16:18 +0700

    slider

--- src/views/Product/index.tsx
+++ src/views/Product/index.tsx
@@ -2,8 +2,19 @@ import { Box, Breadcrumbs, Container, Link } from "@mui/material";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import CustomBreadcrumbs from "@/components/CustomBreadcrumbs";
 import Image from "next/image";
+import MemoizedSliders from "@/components/Sliders";
 
-
+const settingsliders = {
+  arrows: true, // Đảm bảo rằng arrows đã được đặt là true
+  infinite: true,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  // autoplay: true,
+  autoplaySpeed: 1000,
+  prevArrow: true,
+  nextArrow: true,
+};
 
 export default function ProductsPage() {
   return (
@@ -11,12 +22,24 @@ export default function ProductsPage() {
       <Container disableGutters>
         <Box sx={{minHeight: '100vh'}}>
           <CustomBreadcrumbs />
-          <Box sx={{borderRadius: 3, overflow: 'hidden', bgcolor: "#ddd"}}>
+          <Box sx={{borderRadius: 3, bgcolor: "#ddd"}}>
             <div className="relative w-full h-[500px]">
               <Image src={"/image/banner.png"} fill alt="banner" priority quality={100}/>
             </div>
-            <div className="min-h-[500px]">
-
+            <div className="">
+              <MemoizedSliders
+                settings={settingsliders}
+                custom={
+                  Array(7).fill(0).map((data, index) => (
+                    <div
+                      key={index}
+                      className="min-h-[200px] bg-rose-400"
+                    >
+                      {index}
+                    </div>
+                  ))
+                }
+              />
             </div>
           </Box>
         </Box>
